78a6f82770777a6f3d14a0f7d1c99611d9627e2d
Bret Little <bret.little@shopify.com>
2022-05-12 11:35:57 -0400

    Add custom user agents for disabling streaming

--- packages/hydrogen/src/types.ts
+++ packages/hydrogen/src/types.ts
@@ -76,6 +76,7 @@ export type HydrogenConfig = {
   shopify?: ShopifyConfig | ShopifyConfigFetcher;
   serverAnalyticsConnectors?: Array<ServerAnalyticsConnector>;
   session?: (log: Logger) => SessionStorageAdapter;
+  botUserAgents?: Array<string>;
 };
 
 export type ClientHandlerConfig = {
